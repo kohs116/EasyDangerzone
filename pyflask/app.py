717b8e368e7532b4c32303a25b6ae9d3e826dc4b
MinjeongSeo <mathmjseo@khu.ac.kr>
2020-07-09 01:46:41 +0900

    upload source code of project

---
+++ pyflask/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello Flask'
+
+@app.route('/info')
+def info():
+    return 'Info'
+
+#업로드 HTML 렌더링
+@app.route('/upload')
+def render_file():
+   return render_template('upload.html')
+
+#파일 업로드 처리
+@app.route('/fileUpload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      #저장할 경로 + 파일명
+      filename = f.filename
+      path = os.getcwd() + '\\files\\'
+      f.save(path + secure_filename(filename))
+      return 'uploads 디렉토리 -> 파일 업로드 성공!'
+
+if __name__ == '__main__':
+    #서버 실행
+   app.run(debug = True)
